adc15a8ea0def375278157450d6e768b2d229ae3
Arif's Zone <arifulislam.java.c@gmail.com>
2023-08-11 23:12:21 -0700

    main

---
+++ lib/chatmain.dart
@@ -0,0 +1,19 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class chatmain extends StatefulWidget {
+  const chatmain({super.key});
+
+  @override
+  State<chatmain> createState() => _chatmainState();
+}
+
+class _chatmainState extends State<chatmain> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
